096ba6a38bb035a144043a1f5190e1e09a101939
Lewis Smith <lewisthesmith@gmail.com>
2017-06-02 13:39:04 +0100

    Guard statements to prevent crash bug

--- MajorTom.podspec
+++ MajorTom.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MajorTom'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'Easily pull remote config from your server'
 
 # This description is used to generate tags and improve search results.
--- MajorTom/Classes/MajorTom.swift
+++ MajorTom/Classes/MajorTom.swift
@@ -22,14 +22,33 @@ public extension UserDefaults {
         
         let downloadTask = session.downloadTask(with: url) { (url: URL?, response: URLResponse?, error: Error?) in
             guard error == nil else {
+                guard failure != nil && response != nil else {
+                    return
+                }
+                failure!(response!)
+                return
+            }
+            
+            guard response != nil && response?.url != nil else {
+                guard failure != nil else {
+                    return
+                }
                 failure!(response!)
                 return
             }
             
             let dict = NSDictionary(contentsOf: (response?.url)!)
-            self.setValuesForKeys(dict as! [String : Any])
-            self.synchronize()            
             
+            if (dict == nil) {
+                failure!(response)
+                return
+            }
+            self.setValuesForKeys(dict as! [String : Any])
+            self.synchronize()
+            
+            guard success != nil else {
+                return
+            }
             success!(response, dict as! Dictionary<String, Any>)
         }
         
